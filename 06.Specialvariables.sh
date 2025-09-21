8d11d561114ac789d29ff68013b237c3084b23cb
DINESH6677 <dgadila@my.bridgeport.edu>
2025-09-21 09:43:02 -0400

    Updating Variables

--- 06.Specialvariables.sh
+++ 06.Specialvariables.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-echo "to show all the values:$@"
-echo "to display script name:$0"
-echo "current working directory:$PWD"
-echo "displays current user:$USER"
-echo "display home directory of current user:$HOME"
-echo "PID OF SCRIPT:$$"
-echo "to display pid of the last command:$!"
+echo "to show all the values: $@"
+echo "to display script name: $0"
+echo "current working directory: $PWD"
+echo "displays current user: $USER"
+echo "display home directory of current user: $HOME"
+echo "PID OF SCRIPT: $$"
+echo "to display pid of the last command: $!"
