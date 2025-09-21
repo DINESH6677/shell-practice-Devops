d319d9dd0d3bb0b8b0316396b5f0e187da397696
DINESH6677 <dgadila@my.bridgeport.edu>
2025-09-21 08:42:53 -0400

    Updating Variables

--- 02.variables.sh
+++ 02.variables.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-person1=Trump
-person2=Putin
-echo $person1:: Hi $person2 how are you?
-echo $person2:: Hi $person1 i am good what about you?
-echo $person1:: Do we have a deal?
-echo $person2:: YES when we will when we get back the ukraine.
+person1=$1
+person2=$2
+echo $1:: Hi $2 how are you?
+echo $2:: Hi $1 i am good what about you?
+echo $1:: Do we have a deal?
+echo $2:: YES when we will when we get back the ukraine.
