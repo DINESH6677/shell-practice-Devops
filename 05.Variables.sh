6ccc6038ff123e0f3d093121ca671aa6e856e195
DINESH6677 <dgadila@my.bridgeport.edu>
2025-09-21 09:26:58 -0400

    Updating Variables

--- 05.Variables.sh
+++ 05.Variables.sh
@@ -2,6 +2,6 @@
 starttime=$(date +%s)
 sleep 5 
 endtime=$(date +%s)
-Total_time =$(($endtime - $starttime))
+Total_time=$(($endtime - $starttime))
 
 echo "The Time taken to get Executed is : $Total_time seconds"
