ad77c7a8f622de98537f2d4a915b1417e67b2190
DINESH6677 <dgadila@my.bridgeport.edu>
2025-09-23 10:08:33 -0400

    practicing

--- 08.if-else.sh
+++ 08.if-else.sh
@@ -2,7 +2,7 @@
 read Number
 if [ $Number -lt 20 ]; then 
     echo "$Number is less than 20"
-elif [ $Number -eq 20]; then
+elif [ $Number -eq 20 ]; then
     echo "$Number is equal to 20"
 else
     echo "$Number is greater than 20"
