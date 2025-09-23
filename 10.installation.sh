bca7276e041c040cb7851cc4f9c6be0695d84c38
DINESH6677 <dgadila@my.bridgeport.edu>
2025-09-23 11:23:44 -0400

    practicing

--- 10.installation.sh
+++ 10.installation.sh
@@ -3,6 +3,7 @@ USER=$(id -u)
 
 if [ $USER -ne 0 ]; then
     echo "please run the script with root privileges"
+    exit 1
 fi
 
 dnf install mysql -y
@@ -12,4 +13,5 @@ if [ $? -eq 0 ]; then
     echo "Mysql installation is successful"
 else
     echo "Error: Mysql installation is unsuccessful"
+    exit 1
 fi
