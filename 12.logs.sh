f6566a3ba5d58bd30e8fbc9ffb72cb88f0c75804
DINESH6677 <dgadila@my.bridgeport.edu>
2025-09-23 13:27:04 -0400

    Practice

--- 12.logs.sh
+++ 12.logs.sh
@@ -5,12 +5,16 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 W="\e[0m"
+LOGS_Folder="/var/log/shell-script-practice"
+script_name= $(echo $0 | cut -d "." -f1)
+logs_file="$LOGS_Folder/$script_name.log"
+mkdir -p $LOGS_Folder
 
+echo " Script started executing at : $(date)"
 if [ $USER -ne 0 ]; then
     echo -e " $R please run the script with root privileges $N"
     exit 1
 fi
-
 VALIDATE(){
     if [ $1 -eq 0 ]; then
         echo -e "$2 installation is $G successful $N"
@@ -20,7 +24,7 @@ VALIDATE(){
     fi
 }
 
-dnf list installed mysql
+dnf list installed mysql &>>$logs_file
 if [ $? -ne 0 ]; then
     dnf install mysql -y
     VALIDATE $? "MYsql"
@@ -28,7 +32,7 @@ else
     echo -e " $W Mysql is already installed so....$N $Y skipping $N"
 fi
 
-dnf list installed nginx
+dnf list installed nginx &>>$logs_file
 if [ $? -ne 0 ]; then
     dnf install nginx -y
     VALIDATE $? "Nginx"
@@ -36,7 +40,7 @@ else
     echo -e " $W Nginx is already installed so....$N $Y skipping $N"
 fi
 
-dnf list installed python3
+dnf list installed python3 &>>$logs_file
 if [ $? -ne 0 ]; then
     dnf install python3 -y
     VALIDATE $? "Python3"
