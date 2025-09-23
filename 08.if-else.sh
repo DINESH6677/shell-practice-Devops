e5187a24d27b19c10ed1cc64f6e83dbc12e263c3
DINESH6677 <dgadila@my.bridgeport.edu>
2025-09-23 10:06:41 -0400

    practicing

--- 08.if-else.sh
+++ 08.if-else.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 read Number
-if $Number -lt 20; then 
+if [ $Number -lt 20 ]; then 
     echo "$Number is less than 20"
-else if $Number -eq 20; then
-    echo "$Number is equal to 20"
-else;
+else
     echo "$Number is greater than 20"
+fi
